ee1d933f64d67b55bc5a9091a289597256619629
Dmitriy Goldner <keisoko@gmail.com>
2020-11-19 18:28:55 -0500

    Replace variable with len function in python file

--- fibonacci_sequence_generator.py
+++ fibonacci_sequence_generator.py
@@ -4,14 +4,14 @@ def fibonacci_sequence_generator(start, end):
         fiblist.append(fiblist[-1] + fiblist[-2])
     return fiblist
 
+
 fiblist = fibonacci_sequence_generator(start=1, end=19)
-amount_of_numbers = len(fiblist)
 print()
-print(f"The sequence of the {amount_of_numbers} fibonacci numbers is:\n\n{fiblist}")
+print(f"The sequence of the {len(fiblist)} fibonacci numbers is:\n\n{fiblist}")
 
 # Output:
 # The sequence of the 21 fibonacci numbers is:
-    
+
 #     [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
 
 # [Done] exited with code=0 in 0.116 seconds

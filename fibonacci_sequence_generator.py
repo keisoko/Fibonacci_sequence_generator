421c788b493bc498d93e24fcb200f34ef742cf67
Dmitriy Goldner <keisoko@gmail.com>
2020-12-07 16:27:29 -0500

    Replace function call with dictionary and for loop

--- fibonacci_sequence_generator.py
+++ fibonacci_sequence_generator.py
@@ -2,16 +2,13 @@ def fibonacci_sequence_generator(start, end):
     fiblist = [0, 1]
     for _ in range(start, end + 1):
         fiblist.append(fiblist[-1] + fiblist[-2])
-    return fiblist
+    return f"The sequence consisting of the {len(fiblist)} fibonacci numbers is:\n\n{fiblist}"
 
 
-fiblist = fibonacci_sequence_generator(start=1, end=19)
-print()
-print(f"The sequence of the {len(fiblist)} fibonacci numbers is:\n\n{fiblist}")
+fibonacci_sequences = {
+    "first_sequence": {"start": 1, "end": 19},
+    "second_sequence": {"start": 1, "end": 24}
+}
 
-# Output:
-# The sequence of the 21 fibonacci numbers is:
-
-#     [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
-
-# [Done] exited with code=0 in 0.116 seconds
+for key in fibonacci_sequences.keys():
+    print(fibonacci_sequence_generator(**fibonacci_sequences[key]), end="\n\n")

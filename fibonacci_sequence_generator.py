24e82e29fd609eaff9277364c8844259d1e76050
Dmitriy Goldner <keisoko@gmail.com>
2021-11-30 23:52:28 -0500

    Change start variable to the constant variable

--- fibonacci_sequence_generator.py
+++ fibonacci_sequence_generator.py
@@ -1,16 +1,16 @@
 """Fibonacci Sequence Generator"""
 
-def fibonacci_sequence_generator(start: int, end: int) -> str:
+def fibonacci_sequence_generator(START_OF_SEQUENCE: int, end: int) -> str:
     """Generates a list of fibonacci sequence numbers from a given sequence"""
     fiblist = [0, 1]
-    for _ in range(start, end + 1):
+    for _ in range(START_OF_SEQUENCE, end + 1):
         fiblist.append(fiblist[-1] + fiblist[-2])
     return f"The {sequence!r} consist of the {len(fiblist)} Fibonacci numbers:\n\n{fiblist}"
 
 
 fibonacci_sequences = {
-    "first_sequence": {"start": 1, "end": 19},
-    "second_sequence": {"start": 1, "end": 24}
+    "first_sequence": {"START_OF_SEQUENCE": 1, "end": 19},
+    "second_sequence": {"START_OF_SEQUENCE": 1, "end": 24}
 }
 
 sequence = None

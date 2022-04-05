1d4691116762ed38775c0a81a0f4570cb9f35dc7
Dmitriy Goldner <keisoko@gmail.com>
2022-04-05 16:03:11 -0400

    Remove sequence = None line in python file

--- fibonacci_sequence_generator.py
+++ fibonacci_sequence_generator.py
@@ -4,19 +4,15 @@
 def fibonacci_sequence_generator(START_OF_SEQUENCE: int, end: int) -> str:
     """Generates a list of fibonacci sequence numbers from a given sequence"""
     fiblist = [0, 1]
-    fiblist.extend(
-        fiblist[-1] + fiblist[-2] for _ in range(START_OF_SEQUENCE, end + 1)
-    )
+    fiblist.extend(fiblist[-1] + fiblist[-2] for _ in range(START_OF_SEQUENCE, end + 1))
 
     return f"The {sequence!r} consist of the {len(fiblist)} Fibonacci numbers:\n\n{fiblist}"
 
 
 fibonacci_sequences = {
     "first_sequence": {"START_OF_SEQUENCE": 1, "end": 19},
-    "second_sequence": {"START_OF_SEQUENCE": 1, "end": 24}
+    "second_sequence": {"START_OF_SEQUENCE": 1, "end": 24},
 }
-
-sequence = None
 
 for sequence, value in fibonacci_sequences.items():
     print(fibonacci_sequence_generator(**value), end="\n\n")

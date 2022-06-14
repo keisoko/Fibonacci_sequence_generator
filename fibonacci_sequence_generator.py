04f01229088946da2304a33f041d1524f12a09fa
Dmitriy Goldner <keisoko@gmail.com>
2022-06-14 18:40:52 -0400

    Add if condition statement. Change variable's case

--- fibonacci_sequence_generator.py
+++ fibonacci_sequence_generator.py
@@ -3,10 +3,14 @@
 
 def fibonacci_sequence_generator(START_OF_SEQUENCE: int, end: int) -> str:
     """Generates a list of fibonacci sequence numbers from a given sequence"""
-    fiblist = [0, 1]
-    fiblist.extend(fiblist[-1] + fiblist[-2] for _ in range(START_OF_SEQUENCE, end + 1))
+    if START_OF_SEQUENCE < 1:
+        return "None"
+    fib_list = [0, 1]
+    fib_list.extend(
+        fib_list[-1] + fib_list[-2] for _ in range(START_OF_SEQUENCE, end + 1)
+    )
 
-    return f"The given sequence consist of the {len(fiblist)} Fibonacci numbers:\n\n{fiblist}"
+    return f"The given sequence consist of the {len(fib_list)} Fibonacci numbers:\n\n{fib_list}"
 
 
 def main():

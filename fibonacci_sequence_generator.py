ef0c045a13d292961daf4592fc3cc9b9c395f8a3
Dmitriy Goldner <keisoko@gmail.com>
2022-06-16 14:29:29 -0400

    Refactor code to use pprint's  underscore_numbers

--- fibonacci_sequence_generator.py
+++ fibonacci_sequence_generator.py
@@ -1,24 +1,30 @@
 """Fibonacci Sequence Generator"""
+import pprint
 
 
-def fibonacci_sequence_generator(START_OF_SEQUENCE: int, end: int) -> str:
+def fibonacci_sequence_generator(START_OF_SEQUENCE: int, end: int) -> list[int]:
     """Generates a list of fibonacci sequence numbers from a given sequence"""
+
     if START_OF_SEQUENCE < 1:
-        return "None"
+        return []
+
     fib_list = [0, 1]
     fib_list.extend(
         fib_list[-1] + fib_list[-2] for _ in range(START_OF_SEQUENCE, end + 1)
     )
 
-    return f"The given sequence consist of the {len(fib_list)} Fibonacci numbers:\n\n{fib_list}"
+    fib_list.insert(0, len(fib_list))
+
+    return fib_list
 
 
 def main():
     """Main program"""
+    pp = pprint.PrettyPrinter(underscore_numbers=True, width=200)
 
-    print(fibonacci_sequence_generator(START_OF_SEQUENCE=1, end=19), end="\n\n")
-    print(fibonacci_sequence_generator(START_OF_SEQUENCE=1, end=24), end="\n\n")
-    print(fibonacci_sequence_generator(START_OF_SEQUENCE=1, end=22), end="\n\n")
+    pp.pprint(fibonacci_sequence_generator(START_OF_SEQUENCE=1, end=19))
+    pp.pprint(fibonacci_sequence_generator(START_OF_SEQUENCE=1, end=24))
+    pp.pprint(fibonacci_sequence_generator(START_OF_SEQUENCE=1, end=22))
 
 
 if __name__ == "__main__":

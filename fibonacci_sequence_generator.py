9aa6c9480ced75cdb33267702e33aeddeb9176fe
Dmitriy Goldner <keisoko@gmail.com>
2023-06-26 17:30:07 -0400

    Replace custom exception with ValueError. Replace custom exception with ValueError. Refactor if conditional.

--- fibonacci_sequence_generator.py
+++ fibonacci_sequence_generator.py
@@ -12,34 +12,24 @@ class ConstantsNamespace:
     START_OF_SEQUENCE: int = 1
 
 
-constants = ConstantsNamespace()
-
-
-class StartOfSequenceLessThanOne(Exception):
-    """Custom exception class"""
-
-    def __str__(self):
-        return "START_OF_SEQUENCE must be 1"
+constant = ConstantsNamespace()
 
 
 @lru_cache(maxsize=5)
 def fibonacci_sequence_generator(end: int) -> list[int]:
     """Generates a list of fibonacci numbers from a given range."""
 
-    if constants.START_OF_SEQUENCE < 1:
-        raise StartOfSequenceLessThanOne()
+    if constant.START_OF_SEQUENCE < 1 or constant.START_OF_SEQUENCE > 1:
+        raise ValueError("START_OF_SEQUENCE must be 1")
 
     fib_list = [0, 1]
     fib_list.extend(
-        fib_list[-1] + fib_list[-2] for _ in range(constants.START_OF_SEQUENCE, end + 1)
+        fib_list[-1] + fib_list[-2] for _ in range(constant.START_OF_SEQUENCE, end + 1)
     )
-    fib_list.insert(0, len(fib_list))
-
     return fib_list
 
 
 def execute_main() -> None:
-
     custom_printer = pprint.PrettyPrinter(underscore_numbers=True, width=200)
 
     custom_printer.pprint(fibonacci_sequence_generator(end=19))
